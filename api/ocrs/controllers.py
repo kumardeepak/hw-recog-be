736e86bde993fa843c4087907a2cfc4ed94b8cec
kd <0xbeefdead@gmail.com>
2020-01-03 20:51:51 +0530

    integrated vision api, expose single entry point for image processing

--- api/ocrs/controllers.py
+++ api/ocrs/controllers.py
@@ -1,11 +1,17 @@
 from flask import Blueprint, jsonify, request
 from api.ocrs.helpers import get_table_structure
+from api.ocrs.process_image import process_image_with_vision
+import os
 
 controllers = Blueprint('controllers', __name__)
+workspace_dir       = '/Users/kd/Workspace/python/github/handwriting-recognition'
+input_dir           = '/tmp/ngnix'
 
 @controllers.route('/process', methods=['POST'])
 def process_ocr():
-    json_data = request.get_json(force=True)
-    filename = json_data['filename']
-    print('received file [%s] for processing' % (filename))
-    return get_table_structure()
+    json_data           = request.get_json(force=True)
+    filename            = json_data['filename']
+    absolute_filepath   = os.path.join(input_dir, filename)
+    
+    print('received file [%s] for processing is present at [%s]' % (filename, absolute_filepath))
+    return process_image_with_vision(absolute_filepath, workspace_dir)
