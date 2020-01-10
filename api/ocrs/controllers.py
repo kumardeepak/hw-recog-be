b77208fa84a9486cfa45c8b68dbfa383e7da5a2f
kd <0xbeefdead@gmail.com>
2020-01-10 19:47:21 +0530

    added api route

--- api/ocrs/controllers.py
+++ api/ocrs/controllers.py
@@ -2,6 +2,8 @@ from flask import Blueprint, jsonify, request
 from api.ocrs.helpers import get_table_structure
 from api.ocrs.process_image import process_image_with_vision
 from api.ocrs.process_image_v1 import process_image_v1
+from api.ocrs.process_invoice_v1 import process_invoice_v1
+
 
 import os
 
@@ -17,3 +19,13 @@ def process_ocr():
     
     print('received file [%s] for processing is present at [%s]' % (filename, absolute_filepath))
     return process_image_v1(absolute_filepath, workspace_dir)
+
+
+@controllers.route('/invoice', methods=['POST'])
+def process_invoice_ocr():
+    json_data           = request.get_json(force=True)
+    filename            = json_data['filename']
+    absolute_filepath   = os.path.join(input_dir, filename)
+    
+    print('received file [%s] for processing is present at [%s]' % (filename, absolute_filepath))
+    return process_invoice_v1(absolute_filepath, workspace_dir)
