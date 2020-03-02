6df43a1a7c268e96113384c24c5899dcad8a2296
kd <0xbeefdead@gmail.com>
2020-03-02 16:49:44 +0530

    added more post processing steps

--- api/ocrs/process_image_v3.py
+++ api/ocrs/process_image_v3.py
@@ -51,6 +51,12 @@ def cleanup_ocr_text(text):
     text = text.replace('.', '/')
     text = text.replace('(', '')
     text = text.replace(')', '')
+
+    l_text = text.split('/')
+    if len(l_text) == 3 and len(l_text[0]) == 3:
+        l_text[0] = l_text[0][1] + l_text[0][2]
+        return '/'.join(l_text)
+    
     return text
 
 def cleanup_ocr_marks_text(text):
