1925c9f3a11d98494c2e58c0bc59311e8c91e5df
dddhiraj <dhiraj.daga@tarento.com>
2020-04-19 12:29:36 +0530

    added comments for table_roi_extractor

--- api/table_rois/table_extractor.py
+++ api/table_rois/table_extractor.py
@@ -10,6 +10,16 @@ class Table_extractor():
     def __init__(self, filepath, SORT_METHOD='top-to-bottom', MAX_THRESHOLD_VALUE=255, BLOCK_SIZE=15,
                  THRESHOLD_CONSTANT=0, SCALE=15):
 
+        '''
+
+        :param filepath: absolute path of input image file
+        :param SORT_METHOD: order of indexing of cells in a table
+        :param BLOCK_SIZE: size of neighbourhood taken in account for calculating adaptive threshold
+        :param THRESHOLD_CONSTANT: offset used for adaptive thresholding
+        :param SCALE: size of pattern finding kernel (line elements in this case)
+        '''
+
+
         self.image_path = filepath
         self.response = {"response": {"tables": []}}
         self.load_image()
@@ -26,9 +36,10 @@ class Table_extractor():
         self.slate = np.zeros(self.input_image.shape)
 
     def get_table_mask(self):
-
+        #binarization of image
         filtered = cv2.adaptiveThreshold(~self.input_image, self.MAX_THRESHOLD_VALUE, cv2.ADAPTIVE_THRESH_MEAN_C,
                                          cv2.THRESH_BINARY, self.BLOCK_SIZE, self.THRESHOLD_CONSTANT)
+        #Finding srtuctre elements (horizontal and vertical lines)
         horizontal = filtered.copy()
         vertical = filtered.copy()
 
@@ -42,7 +53,9 @@ class Table_extractor():
         vertical = cv2.erode(vertical, vertical_structure)
         vertical = cv2.dilate(vertical, vertical_structure)
 
+        #generating table borders
         self.mask = horizontal + vertical
+
 
     def sort_contours(self, cnts, method="left-to-right"):
         reverse = False
@@ -55,34 +68,43 @@ class Table_extractor():
         (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes), key=lambda b: b[1][i], reverse=reverse))
         return (cnts, boundingBoxes)
 
+
     def draw_contours_index(self, contours, img):
+        '''
+
+        :param contours:  contours present cropped fraction of mask image
+        :param img: cropped portion of mask image having one table (in case when input image has multiple tables )
+        :return: image indexed with cell location, list of bounding box coordinates of every individual cell
+        '''
         image_area = img.shape[0] * img.shape[1]
         draw_conts = np.zeros(img.shape)
-        margin = 10
+        #margin = 10
         midpoints = []
         rects = []
         xi, yi = 0, 0
         for i in range(len(contours)):
-            cont_area = area = cv2.contourArea(contours[i])
+            cont_area = cv2.contourArea(contours[i])
             x1, y1, w1, h1 = cv2.boundingRect(contours[i])
 
+            #
             if cont_area / float(image_area) < 0.9:
                 midpoint = [int(x1 + w1 / 2), int(y1 + h1 / 2)]  # np.mean(contours[i],axis=0)
                 midpoints.append(midpoint)
                 if len(midpoints) > 1:
                     shift = midpoints[-1][0] - midpoints[-2][0]
+
+                    # Detecting change in column by measuring difference in x coordinate of current and previous cell
+                    # (cells already sored based on their coordinates)
                     if shift < 10:
                         # print(shift , midpoints[-1][0] , midpoints[-2][0] ,'True')
                         # print(xi,yi)
                         yi = yi + 1
                     else:
-
                         yi = 0
                         xi = xi + 1
                         # print('False' ,xi,yi)
                 rects.append({"x": x1, "y": y1, "w": w1, "h": h1, "index": (xi, yi)})
                 cv2.rectangle(draw_conts, (x1, y1), (x1 + w1, y1 + h1), 255, 1)
-                # draw_conts = cv2.drawContours( draw_conts, contours[i], -1, 255, 3)
                 cv2.putText(draw_conts, str((xi, yi)), (int(midpoint[0]), int(midpoint[1])), cv2.FONT_HERSHEY_SIMPLEX,
                             0.3, 255, 1, cv2.LINE_AA)
         return draw_conts, rects
@@ -91,17 +113,19 @@ class Table_extractor():
 
         # list_of_tables = []
         image_area = float(self.input_image.shape[0] * self.input_image.shape[1])
-        # finiding all the tables in the image
+
+        # finding all the tables in the image, cv2.RETR_EXTERNAL gives only the outermost border of an
+        # enclosed figure.
         contours = cv2.findContours(self.mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         contours = contours[0] if len(contours) == 2 else contours[1]
 
         if len(contours) > 0:
-            table_index = {}
-            table_dic = {}
+            # Indexing one table at a time
             for c in contours:
-
                 x, y, w, h = cv2.boundingRect(c)
                 area_ratio = (w * h) / image_area
+
+                # Filtering for noise
                 if (area_ratio < 0.8) & (area_ratio > 0.05):
                     table_dic = {"x": x, "y": y, "w": w, "h": h}
 
@@ -115,5 +139,7 @@ class Table_extractor():
                     indexed_sub_image, rects = self.draw_contours_index(sorted_conts, img=crop_fraction)
                     table_dic['rect'] = rects
 
+                    # self.slate stores an image indexed with cell location for all available tables
                     self.slate[y - 2: y + h + 2, x - 2:x + w + 2] = indexed_sub_image
+
                     self.response["response"]["tables"].append(table_dic)
