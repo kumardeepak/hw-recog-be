a9e9e52ab51b90a70bca2d1739babd298e457998
dddhiraj <dhiraj.daga@tarento.com>
2020-05-15 11:09:46 +0530

    crop margin changed form 5 to 10

--- src/repositories/bbox_tools.py
+++ src/repositories/bbox_tools.py
@@ -126,7 +126,7 @@ class Box_cordinates:
                 if text_crop['height'] > mean_height * 0.5 :
                     if text_crop['fragmented'] :
                         for word in text_crop['fragments']:
-                            cropped_portion = self.crop_im(word,margin=5)
+                            cropped_portion = self.crop_im(word,margin=10)
                             text = pytesseract.image_to_data(cropped_portion,config='--psm 7', lang='eng',output_type=Output.DATAFRAME)
                             text = text[text['conf'] >self.conf_threshold]
                             if len(text) > 0 :
