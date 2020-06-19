fed8c5b5b0ab1296b051ebafd6a623a1fe0c8ac1
dhiraj <dhiraj.daga@tarento.com>
2020-06-19 13:46:03 +0530

    resolve route error

--- src/resources/__init__.py
+++ src/resources/__init__.py
@@ -1,4 +1,4 @@
 from .rect import RectResource
 from .rect import OcrLineResourcev1
 from .rect import OcrLineResourcev2
-from .rect import OCRlineRepositoriesv3
+from .rect import OcrLineResourcev3
